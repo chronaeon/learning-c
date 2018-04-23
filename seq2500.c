e96cd43f1042c56830e1bc58053528dc2121ccd0
chronaeon <micahdameron@gmail.com>
2018-04-23 10:35:19 -0400

    new file:   seq2500.c 	new file:   seq2500GCC-S.s

---
+++ seq2500.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){;
+
+
+int fart = 1;
+
+	for(fart=1; fart <= 2500000; fart++)
+	{
+		printf("%d\n", fart);
+	}
+return 0;
+
+}
---
+++ seq2500GCC-S.s
@@ -0,0 +1,38 @@
+	.file	"test.c"
+	.section	.rodata
+.LC0:
+	.string	"%d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$1, -4(%rbp)
+	movl	$1, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	addl	$1, -4(%rbp)
+.L2:
+	cmpl	$2500000, -4(%rbp)
+	jle	.L3
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.2.0-8ubuntu3.2) 7.2.0"
+	.section	.note.GNU-stack,"",@progbits
